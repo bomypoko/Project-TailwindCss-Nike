538b6d1df316c5fb67f217da5249c39cce2c1fba
Ratchasri Kidking <orc_hero@hotmail.com>
2023-09-21 16:43:44 +0700

    Set Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,33 @@ import React from 'react'
 
 const App = () => {
   return (
-   <main className='p-5 '>
-    <h1 className='text-center'>Hello world</h1>
-    <div className='bg-black text-white h-10 rounded-xl my-4 p-2'>
-      <p className='text-center '> a div</p>
-    </div>
-
-    <div className='flex justify-center space-x-3 first-line:'>
-      <div className='h-16 w-16 bg-blue-500 rounded-full'></div>
-      <div className='h-16 w-16 bg-blue-500 rounded-full'></div>
-      <div className='h-16 w-16 bg-blue-500 rounded-full'></div>
-    </div>
-
-    <div className='grid grid-cols-3 mt-3 gap-2'>
-      <div className='bg-red-300 h-12'></div>
-      <div className='bg-red-300 h-12'></div>
-      <div className='bg-red-300 h-12'></div>
-    </div>
-    <div className='hidden md:block'>
-      <p>i will appear a device above 768px</p>
-    </div>
-    <div className='max-md:block hidden'>
-      <p>I will appear a device below 1111</p>
-    </div>
-   </main>
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:bg-red-300 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='padding'>
+        PopularProduct
+      </section>
+      <section className='padding'>
+        SuperQuality
+      </section>
+      <section className='padding'>
+        Services
+      </section>
+      <section className='padding'>
+        SpecialOffer
+      </section>
+      <section className='bg-pale-blue padding'>
+        Customer Review
+      </section>
+      <section className='padding-x py-16 sm:py-32 bg-red-300 w-full'>
+        Subscribe
+      </section>
+      <section className='bg-black padding-t pb-8'>
+        Footer
+      </section>
+    </main>
   )
 }
 
