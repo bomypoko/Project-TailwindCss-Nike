8708aefb5686d9fa9f8249ac90d2dac9594d4e1b
Ratchasri Kidking <orc_hero@hotmail.com>
2023-09-22 15:03:51 +0700

    Changing Web Icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { Hero , CustomerReviews , Footer, PopularProducts, Services , SpecialOffer, Subscribe , SuperQuality } from './sections/index'
-
+import Nav from './components/Nav'
 const App = () => {
   return (
     <main className='relative'>
-      Nav
+      <Nav/>
       <section className='xl:padding-1 wide:bg-red-300 wide:padding-r padding-b'>
         <Hero/>
       </section>
       <section className='padding'>
-        PopularProduct
+        <PopularProducts/>
       </section>
       <section className='padding'>
-        SuperQuality
+        <SuperQuality/>
       </section>
       <section className='padding'>
-        Services
+        <Services/>
       </section>
       <section className='padding'>
-        SpecialOffer
+        <SpecialOffer/>
       </section>
       <section className='bg-pale-blue padding'>
-        Customer Review
+        <CustomerReviews/>
       </section>
       <section className='padding-x py-16 sm:py-32 bg-red-300 w-full'>
-        Subscribe
+        <Subscribe/>
       </section>
       <section className='bg-black padding-t pb-8'>
-        Footer
+        <Footer/>
       </section>
     </main>
   )
