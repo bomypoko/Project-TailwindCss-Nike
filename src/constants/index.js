4e1502229d4362e70dd21eefb5fef13675615aea
Ratchasri Kidking <orc_hero@hotmail.com>
2023-10-12 15:25:13 +0700

    Add Statistic

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,12 @@ export const navLinks = [
     {href: "#contact",label: "Contact"},
     {href: "#product",label: "Product"},
     {href: "#contact-us",label: "Contact Us"}
-]
+];
+
+
+
+export const statistics = [
+    {id : '1+' , value : '1K+', label: "Brands"},
+    {id : '2' , value : '500+', label: "Shop"},
+    {id : '3' , value : '250K' , label: "Customers"}
+];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
+import { statistics } from '../constants'
+
 
 const Hero = () => {
   return (
     <section id="home"className='w-full flex xl:flex-row flex-col p-2 border-2 border-red-300 justify-center gap-10 min-h-screen max-container'>
         <div className='relative flex flex-col justify-center items-start  w-full max-xl:padding-x pt-28'>
           <p>Our Summer Collections</p>
-          <h1> <span>The New Arrival</span>
-            <br />
-            <span>Nike</span> 
-          </h1>
+          <h1> <span>The New Arrival</span> <br/> <span>Nike Shoes</span> </h1>
           <p>Discover stylish Nike arrivals,quality comfort , and innovation for your active life.</p>
           
           {/* Button Component passing label and iconURL to the component */}
@@ -18,10 +17,20 @@ const Hero = () => {
             label="Shop Now"
             iconURL={arrowRight}
           />
-        </div>  
-
-       
-       
+          <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          
+          {/* import statistics from Constants */}
+          {statistics.map((item) => (
+            
+            <div key={item.id}>
+              <p>{item.value}</p>
+              <p>{item.label}</p>
+              
+            </div>
+          ))}
+         
+          </div>
+        </div>
     </section>
   )
 }
