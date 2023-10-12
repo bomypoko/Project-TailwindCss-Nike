8b391e662701d40288ded2a02e40a350fd9f2e38
Ratchasri Kidking <orc_hero@hotmail.com>
2023-10-12 14:37:18 +0700

    start hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero Section</div>
+    <section id="home"className='w-full flex xl:flex-row flex-col p-2 border-2 border-red-300 justify-center gap-10 min-h-screen max-container'>
+        <div className='relative flex flex-col justify-center items-start  w-full max-xl:padding-x pt-28'>
+          <p>Our Summer Collections</p>
+          <h1>
+            <span>The New Arrival</span>
+            <br />
+            <span>Nike</span> 
+          </h1>
+          <p>Discover stylish Nike arrivals,quality comfort , and innovation for your active life.</p>
+        </div>  
+       
+    </section>
   )
 }
 
